66e354b5f5fb5955a77c6c68a41b482662a1d0d8
yang <kiki9510@gmail.com>
2021-08-20 17:40:42 +0900

    Added CategorySerializer and CategoryDetail ApiView.

--- product/views.py
+++ product/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
 
 # 최근 추가된 상품 리스트(최신순으로 4개만 보여준다)
@@ -26,4 +26,17 @@ class ProductDetail(APIView):
     def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
